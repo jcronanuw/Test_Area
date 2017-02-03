6f7c147c6e913834d755f2ece6d5835575285f2c
jcronanuw <jcronan@uw.edu>
2017-02-03 10:28:04 -0800

    Populate overstory table with ID numbers

--- overstory_analysis.R
+++ overstory_analysis.R
@@ -4,3 +4,8 @@ overstory.table <- read.table("overstory_table.csv", header=TRUE,
                          sep=",", na.strings="NA", dec=".", strip.white=TRUE)
 
 
+
+overstory.table <- matrix(data = c(1:10000, rep(0, 10000), rep(0, 10000)), 10000, 3)
+
+
+
