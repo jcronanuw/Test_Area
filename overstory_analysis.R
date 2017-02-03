01cda2386fc90cc43bbeec5c757a06aca4630af3
jcronanuw <jcronan@uw.edu>
2017-02-03 09:59:58 -0800

    Add R script for overstory analysis

---
+++ overstory_analysis.R
@@ -0,0 +1,6 @@
+
+setwd("C:/Users/jcronan/Documents/GitHub/Test_Area/")
+overstory.table <- read.table("overstory_table.csv", header=TRUE, 
+                         sep=",", na.strings="NA", dec=".", strip.white=TRUE)
+
+
