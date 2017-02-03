508b4affe8a30e1a856afaedfa5596ac7282b0b4
jcronanuw <jcronan@uw.edu>
2017-02-03 14:00:26 -0800

    Add code to script that will populate template overstory table.

--- overstory_analysis.R
+++ overstory_analysis.R
@@ -1,11 +1,48 @@
+#START
 
+#Purpose of this script is to serve as dummy data for GitHub testing
+
+#Set working drive
 setwd("C:/Users/jcronan/Documents/GitHub/Test_Area/")
+
+#Import input table template.
 overstory.table <- read.table("overstory_table.csv", header=TRUE, 
                          sep=",", na.strings="NA", dec=".", strip.white=TRUE)
 
 
-
+#Format the input table and add ID numbers for 10,000 trees.
 overstory.table <- matrix(data = c(1:10000, rep(0, 10000), rep(0, 10000)), 10000, 3)
 
 
+##re-add the column names that were stripped when you turned the table into a matrix.
+colnames(overstory.table) <- c("ID_number", "Diameter_in", "Height_ft")
+
+#Convert the data table into a data frame.
+overstory.table <- as.data.frame(overstory.table)
+
+#Use the negative exponential distribution to populate the table with DBH values.
+overstory.table$Diameter_in <- round(rexp(10000, rate =0.2 ),1)
+
+#Create a function that will predict height and incorporate variability.
+pred_ht <- function(x,y)
+{
+  round(rnorm(1, 4.5 + exp(7.26 + -5.89*(x^-0.28)), y),1)
+}
+#where x is diameter at breast height (DBH) in inches and y is standard deviation
+#equation form is from Curtis 1967
+#coefficients are from Hanus et al. 1999
+
+#Populate data table with tree height data.
+overstory.table$Height_ft <- sapply(overstory.table$Diameter_in, pred_ht, y = 1)
+
+#Identify date and time.
+dt <- Sys.Date()
+tm <- format(Sys.time(), format = "%H.%M.%S", 
+             tz = "", usetz = FALSE)
+
+#Save file
+write.csv(overstory.table, file = paste("output_file_on_",dt,"_",tm,".csv",sep = ""), 
+          quote = TRUE, eol = "\n", na = "NA", row.names = FALSE)
+
+#END
 
